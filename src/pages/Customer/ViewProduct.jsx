f0db801f1a81c8cd96f8713f6270df11f6e6cbf2
abdullah <abdullamsuleman@gmail.com>
2026-08-25 23:01:04 +0300

    fetch product by id ViewProduct

--- src/pages/Customer/ViewProduct.jsx
+++ src/pages/Customer/ViewProduct.jsx
@@ -1,10 +1,20 @@
 import { useParams } from "react-router"
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import { show } from "../../services/productService"
 
 const ViewProduct = () => {
 const { id } = useParams()
 const [product, setProduct] = useState(null)
 const [loading, setLoading] = useState(true)
+
+useEffect(() => {
+const fetchProduct = async () => {
+const data = await show(id)
+    setProduct(data)
+    setLoading(false)
+}
+    fetchProduct()
+}, [id])
 
 return (
     <section>
