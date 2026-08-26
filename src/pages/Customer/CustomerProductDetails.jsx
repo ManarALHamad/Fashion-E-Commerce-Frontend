c5eb65b2df6d4f546e64979eb26dd5409b406749
abdullah <abdullamsuleman@gmail.com>
2026-08-26 16:38:46 +0300

    preselect the first variant on load

--- src/pages/Customer/CustomerProductDetails.jsx
+++ src/pages/Customer/CustomerProductDetails.jsx
@@ -8,12 +8,17 @@ const CustomerProductDetails = () => {
 const { productId } = useParams
 const [product, setProduct] = useState(null)
 const [loading, setLoading] = useState(true)
+const [selectedImage, setSelectedImage] = useState(0)
+const [selectedVariant, setSelectedVariant] = useState(null)
 
 useEffect(() =>{
     const fetchProduct = async () => {
         try {
         const data = await show(productId)
         setProduct(data)
+
+    if (data.variants.length > 0) setSelectedVariant(data.variants[0])
+
         setLoading(false)
         } catch (error) {
             console.log(error)
