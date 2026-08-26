de466bd45bdc398b4d6edf2baf604f66b6572cdf
abdullah <abdullamsuleman@gmail.com>
2026-08-26 15:52:11 +0300

    Customer details useState

--- src/pages/Customer/CustomerProductDetails.jsx
+++ src/pages/Customer/CustomerProductDetails.jsx
@@ -1,9 +1,13 @@
 import { useParams } from 'react-router'
 import { useState, useEffect } from 'react'
+import { show } from '../../services/productService'
 
+const BASE_URL = import.meta.env.VITE_BACK_END_SERVER_URL
 
 const CustomerProductDetails = () => {
 const { productId } = useParams
+const [product, setProduct] = useState(null)
+const [loading, setLoading] = useState(true)
 
     return <main>Customer Details</main>
 }
