8f40ef18ec589e315135078d0174b991504c1239
abdullah <abdullamsuleman@gmail.com>
2026-08-27 01:00:02 +0300

    cart service

--- src/services/cartService.js
+++ src/services/cartService.js
@@ -1,3 +1,12 @@
-const getCart = ()=> {
-
+const getCart = () => {
+  const savedCart = localStorage.getItem("cart")
+  return savedCart ? JSON.parse(savedCart) : []
 }
+
+const addToCart = (item) => {
+  const cart = getCart()
+  cart.push(item)
+  localStorage.setItem("cart", JSON.stringify(cart))
+}
+
+export { getCart, addToCart }
