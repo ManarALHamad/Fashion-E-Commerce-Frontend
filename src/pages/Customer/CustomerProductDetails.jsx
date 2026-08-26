b2c9d3bad4e1673c73de8eda256604a9eb78d232
abdullah <abdullamsuleman@gmail.com>
2026-08-26 15:44:24 +0300

    Customer details useParams

--- src/pages/Customer/CustomerProductDetails.jsx
+++ src/pages/Customer/CustomerProductDetails.jsx
@@ -1,5 +1,11 @@
-const CustomerProductDetails = () => {
+import { useParams } from 'react-router'
+import { useState, useEffect } from 'react'
 
+
+const CustomerProductDetails = () => {
+const { productId } = useParams
+
+    return <main>Customer Details</main>
 }
 
 export default CustomerProductDetails
