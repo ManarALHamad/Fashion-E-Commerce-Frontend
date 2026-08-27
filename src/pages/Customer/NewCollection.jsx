966d97fd0f932b513d98e914381d1ce0580c566c
abdullah <abdullamsuleman@gmail.com>
2026-08-27 12:58:56 +0300

    All the imports in newCollection.jsx

--- src/pages/Customer/NewCollection.jsx
+++ src/pages/Customer/NewCollection.jsx
@@ -1,3 +1,7 @@
+import { useState, useEffect } from "react"
+import { index as indexProducts } from "../../services/productService"
+import { index as indexSubcategories } from "../../services/subcategoryService"
+
 const NewCollection = () => {
 
 }
