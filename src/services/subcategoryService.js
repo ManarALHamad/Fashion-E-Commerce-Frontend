8c651abb8fad5ca4c54acf6db2ec7c9ad8149e9f
abdullah <abdullamsuleman@gmail.com>
2026-08-30 16:29:13 +0300

    (fixes)

--- src/services/subcategoryService.js
+++ src/services/subcategoryService.js
@@ -3,7 +3,7 @@ const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/subcategories`
 const index = async() => {
 try {
     const res = await fetch(BASE_URL, {
-    headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+    // headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
 })
 return res.json()
 } catch (error) {
