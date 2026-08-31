00f3eed171997d8e1f2a55e48883d97652934071
Hawra <hawraasharaf89@gmail.com>
2026-08-31 11:04:44 +0300

    contact info in contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,19 @@
+
+
 const Contact = () => {
     return(
-        <h1>Contact</h1>
+        <>
+            <h1>Contact</h1>
+            <button>
+                <img src="/src/assets/images/instagram.png" alt="Instagram icon" width="20px" />
+                <a href="https://www.instagram.com/n.designs.bh/">n.designs.bh</a>
+            </button>
+            <br />
+            <button>
+                <img src="/src/assets/images/whatsapp.png" alt="WhatsApp icon" width="20px" />
+                <a href="https://wa.me/97332266677">32266677</a>
+            </button>
+        </>
     )
 }
 
